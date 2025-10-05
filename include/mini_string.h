6b574ec54e1554eb0ed4061e8e5433a448de1ae4
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-10-05 15:15:49 +0300

    I enhanced  the strlen to be comparative to strnlen()to ensure safe buffer.

--- include/mini_string.h
+++ include/mini_string.h
@@ -5,7 +5,7 @@
 
 #include <stddef.h> //for size_t
 
-size_t my_strlen(const char *s);
+size_t my_strnlen(const char *s, size_t maxlen);
 char *my_strcpy(char *dest, const char *src);
 int my_strcmp(const char *s1, const char *s2);
 
