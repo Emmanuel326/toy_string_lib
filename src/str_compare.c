088c6829e7a7fc1b8dc5ebca8d27d84532dd3c4e
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-10-06 09:51:34 +0300

    I added the string compare function helped with ASCII and string comparison in c

---
+++ src/str_compare.c
@@ -0,0 +1,16 @@
+#include "../include/mini_string.h"
+
+int my_strncmp(const char  *s1, const char *s2)
+{
+  while (n && *s1 && (*s1 == *s2)){
+  
+	  s1++;
+	  s2++;
+	  n--;
+  }
+  
+  if (n==0)
+	  return 0;
+  return(unsigned char)*s1 -(unsigned char)*s2;
+}
+
---
+++ src/str_copy.c
@@ -0,0 +1,15 @@
+#include "../include/mini_string.h"
+
+
+
+size_t my_strcpy(char *dest, const char *src, size_t size)
+{
+  size_t i = 0;
+  //if the size is greater than zero, copy upto that size -1
+  if(n != 0){
+    while (--n != )
+
+  }
+}
+
+
